2e2e6e226a96d1b4403c10f965de40c994ca595f
night780 <night780j@gmail.com>
2022-05-05 20:39:32 -0400

    routing/var edits & bug fixes

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 ini_set('display_errors',1);
 error_reporting(E_ALL);
 
+//Starts session
+session_start();
+
 //Require the autoload file
 require_once('vendor/autoload.php');
 
@@ -12,17 +15,12 @@ $f3=Base::instance();
 $f3->route('GET /', function(){
     $view = new Template();
     echo $view->render('views/home.html');
-}
-);
-
+});
 //Define a home route
 $f3->route('GET /home', function(){
     $view = new Template();
     echo $view->render('views/home.html');
-}
-);
-
-
+});
 //Define a Features route
 $f3->route('GET /Features', function(){
 
@@ -35,18 +33,49 @@ $f3->route('GET /entry', function(){
     $view = new Template();
     echo $view->render('views/entry.html');
 });
-
 //Define a LoginPage route
 $f3->route('GET /LoginPage', function(){
 
     $view = new Template();
     echo $view->render('views/LoginPage.html');
-}
-);
+});
 //Define a Pricing route
 $f3->route('GET /Pricing', function(){
     $view = new Template();
     echo $view->render('views/Pricing.html');
+});
+//Define a createProfile route
+$f3->route('GET /CreateProfile', function(){
+
+    $view = new Template();
+    echo $view->render('views/createProfile.html');
+});
+//Define a createProfile2 route
+$f3->route('POST /CreateProfile2', function(){
+    var_dump($_POST);
+    $_SESSION['fname']=$_POST['fname'];
+    $_SESSION['lname']=$_POST['lname'];
+    $_SESSION['Age']=$_POST['Age'];
+    $_SESSION['gridRadios']=$_POST['gridRadios'];
+    $_SESSION['Phone']=$_POST['Phone'];
+
+    $view = new Template();
+    echo $view->render('views/createProfile2.html');
+});
+//Define a createProfile3 route
+$f3->route('POST /CreateProfile3', function(){
+    var_dump($_POST);
+    $_SESSION['fname']=$_POST['fname'];
+    $_SESSION['lname']=$_POST['lname'];
+    $_SESSION['Age']=$_POST['Age'];
+    $_SESSION['gridRadios']=$_POST['gridRadios'];
+    $view = new Template();
+    echo $view->render('views/createProfile3.html');
+});
+//Define a summary route
+$f3->route('GET|POST /summary', function(){
+    $view = new Template();
+    echo $view->render('views/Summary.html');
 });
 
 //runs fat free
